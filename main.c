28747e5166172687351ecd94c5ca96bb98eed83a
Diegusho <diego.negrinramirez@gmail.com>
2024-05-23 20:19:59 -0400

    Agregado test 2

--- main.c
+++ main.c
@@ -4,8 +4,62 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+struct Fecha {
+    int dia;
+    int mes;
+    int year;
+};
+
+struct Lote {
+    int numeroLote;
+    int cantidadLote;
+    struct Fecha *fechaCaducidad;
+};
+
+struct Producto {
+    int codigo;
+    char *nombre;
+    char *categoria;
+    int precio;
+    char *descripcion;
+    char *proveedor;
+    int lote;
+    int cantidad;
+    int requiereReceta;
+    struct Lote *lotes;
+};
+
+struct NodoProducto {
+    struct DetallesProducto *producto;
+    struct NodoProducto *izq, *der;
+};
+
+struct CompraVenta {
+    char *nombre;
+    int cantidadProductos;
+    struct Producto **productos;
+};
+
+struct NodoCompraVenta {
+    struct CompraVenta *producto;
+    struct NodoCompraVenta *sig;
+};
+
+struct Farmacia {
+    char id[10];
+    char *ciudad;
+    char *region;
+    struct NodoProducto *inventario;
+    int totalProductos;
+    struct NodoCompraVenta **ventas, **compras;
+};
+
+struct NodoFarmacia {
+    struct Farmacia *datosFarmacia;
+    struct NodoFarmacia *ant, *sig;
+};
+
 int main(void) {
-    printf("Hello World");
-    printf("Test 2")
+
     return 0;
 }
