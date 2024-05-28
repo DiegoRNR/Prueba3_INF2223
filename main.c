a267cf94ed69ac6d2a9dbb65218db8fa60daaaaf
Diegusho <diego.negrinramirez@gmail.com>
2024-05-28 01:42:58 -0400

    Agregadas mas funciones básicas

--- main.c
+++ main.c
@@ -4,6 +4,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
 #define CAPACIDAD_PROMEDIO 10000
 
 struct Fecha {
@@ -68,25 +69,59 @@ struct NodoFarmacia {
     struct NodoFarmacia *ant, *sig;
 };
 
+struct FarmaSalud {
+    struct NodoFarmacia *headFarmacias;
+};
+
 char *leerCadena() {
-    char buffer[100], caracter, *cadena = NULL;
+    char buffer[100], caracter, *cadena;
     int i;
+
     for (i = 0; i < 100 && caracter != '\0'; i++) {
         caracter = getchar();
         if (caracter == '\n')
             caracter = '\0';
         buffer[i] = caracter;
     }
+    cadena = (char *) malloc((sizeof(char))*(strlen(buffer) + 1));
     strcpy(cadena, buffer);
     return cadena;
 }
 
 struct Fecha *leerFecha() {
     struct Fecha *fecha;
-    int dia, mes, year;
-    scanf("%d", &dia);
-    scanf("%d", &mes);
-    scanf("%d", &year);
+    struct tm *tiempoLocal;
+    time_t tiempoActual;
+    int dia, mes, year, opcion;
+
+    do {
+        printf("\nElija modo de ingreso de fecha:\n");
+        printf("1. Fecha Actual             2. Fecha Personalizada\n");
+        printf("Ingrese la opcion que desea realizar (1-2): ");
+        scanf("%d", &opcion);
+        switch (opcion) {
+            case 1:
+                time(&tiempoActual);
+                tiempoLocal = localtime(&tiempoActual);
+                dia = tiempoLocal->tm_mday;
+                mes = tiempoLocal->tm_mon + 1;
+                year = tiempoLocal->tm_year + 1900;
+                break;
+            case 2:
+                printf("Ingrese fecha (DD/MM/AAAA): ");
+                scanf("%d", &dia);
+                getchar();
+                scanf("%d", &mes);
+                getchar();
+                scanf("%d", &year);
+                getchar();
+                break;
+            default:
+                printf("Opcion invalida, por favor ingrese una opcion valida.\n\n");
+                break;
+        }
+    } while (opcion < 1 || opcion > 2);
+
     fecha = (struct Fecha *) malloc(sizeof(struct Fecha));
     fecha->dia = dia;
     fecha->mes = mes;
@@ -97,15 +132,18 @@ struct Fecha *leerFecha() {
 struct Farmacia *crearFarmacia() {
     struct Farmacia *nuevaFarmacia;
     char *id, *ciudad, *region;
-    int totalProductos;
+
     region = leerCadena();
     ciudad = leerCadena();
     id = leerCadena();
+
     nuevaFarmacia = (struct Farmacia *) malloc(sizeof(struct Farmacia));
     nuevaFarmacia->id = id;
     nuevaFarmacia->ciudad = ciudad;
     nuevaFarmacia->region = region;
     nuevaFarmacia->maxCapacidad = CAPACIDAD_PROMEDIO;
+    nuevaFarmacia->inventario = NULL;
+    nuevaFarmacia->totalProductos = 0;
     nuevaFarmacia->ventas = NULL;
     nuevaFarmacia->compras = NULL;
     return nuevaFarmacia;
@@ -122,17 +160,56 @@ struct NodoFarmacia *crearNodoFarmacia(struct Farmacia *farmacia) {
     return nuevaFarmacia;
 }
 
+struct Farmacia *getFarmacia(struct NodoFarmacia *head, char *idBuscado) {
+    struct NodoFarmacia *rec;
+    if (head != NULL) {
+        rec = head;
+        do {
+            if (strcmp(rec->datosFarmacia->id, idBuscado) == 0)
+                return rec->datosFarmacia;
+            rec = rec->sig;
+        } while (rec != head);
+    }
+    return NULL;
+}
+
+int agregarNodoFarmacia(struct NodoFarmacia **head, struct NodoFarmacia *nuevoNodo) {
+    if (*head == NULL) {
+        *head = nuevoNodo;
+        (*head)->ant = nuevoNodo;
+        (*head)->sig = nuevoNodo;
+        return 1;
+    }
+    if (getFarmacia(*head, nuevoNodo->datosFarmacia->id) == NULL) {
+        nuevoNodo->sig = *head;
+        nuevoNodo->ant = (*head)->ant;
+        (*head)->ant->sig = nuevoNodo;
+        (*head)->ant = nuevoNodo;
+        return 1;
+    }
+    return 0;
+}
+
 struct Producto *crearProducto() {
     struct Producto *nuevoProducto;
-    char *nombre, *categoria, *descripcion, *proveedor, *codigo;
+    char *nombre, *categoria, *descripcion, *proveedor, codigo[11];
     int precio, requiereReceta;
+
+    printf("Nombre del producto: ");
     nombre = leerCadena();
+    printf("Categoria del producto: ");
     categoria = leerCadena();
+    printf("Descripcion del producto: ");
     descripcion = leerCadena();
+    printf("Proveedor del producto: ");
     proveedor = leerCadena();
-    codigo = leerCadena();
+    printf("Codigo del producto (10 digitos): ");
+    strcpy(codigo, leerCadena());
+    printf("Precio del producto: ");
     scanf("%d", &precio);
+    printf("Producto requiere receta (0-1): ");
     scanf("%d", &requiereReceta);
+
     nuevoProducto = (struct Producto *) malloc(sizeof(struct Producto));
     nuevoProducto->nombre = nombre;
     nuevoProducto->categoria = categoria;
@@ -157,13 +234,72 @@ struct NodoProducto *crearNodoProducto(struct Producto *producto) {
     return nuevoNodo;
 }
 
+int compararCodigoProductos(char *codigo1, char *codigo2) {
+    char digito1[] = "\0\0", digito2[] = "\0\0";
+    int i;
+    for (i = 0; i < 10; i++) {
+        digito1[0] = codigo1[i];
+        digito2[0] = codigo2[i];
+        if (strcmp(digito1, digito2) != 0)
+            return strcmp(digito1, digito2);
+    }
+    return 0;
+}
+
+struct Producto *getProducto(struct NodoProducto *root, char *codigoBuscado) {
+    if (!root)
+        return NULL;
+    if (strcmp(root->datosProducto->codigo, codigoBuscado) == 0)
+        return root->datosProducto;
+    if (compararCodigoProductos(root->datosProducto->codigo, codigoBuscado) > 0)
+        return getProducto(root->izq, codigoBuscado);
+    return getProducto(root->der, codigoBuscado);
+}
+
+struct NodoProducto *getPadreACodigo(struct NodoProducto *root, char *codigo) {
+    if (strcmp(root->datosProducto->codigo, codigo) == 0)
+        return NULL;
+    if (compararCodigoProductos(root->datosProducto->codigo, codigo) > 0) {
+        if (root->izq == NULL || compararCodigoProductos(root->izq->datosProducto->codigo, codigo) == 0)
+            return root;
+        return getPadreACodigo(root->izq, codigo);
+    }
+    if (root->der == NULL || compararCodigoProductos(root->der->datosProducto->codigo, codigo) == 0)
+        return root;
+    return getPadreACodigo(root->der, codigo);
+}
+
+int agregarNodoProducto(struct NodoProducto **root, struct NodoProducto *nuevoNodo) {
+    struct NodoProducto *nodoPadre;
+    if (*root == NULL) {
+        *root = nuevoNodo;
+        return 1;
+    }
+    if (getProducto(*root, nuevoNodo->datosProducto->codigo) == NULL) {
+        nodoPadre = getPadreACodigo(*root, nuevoNodo->datosProducto->codigo);
+        if (compararCodigoProductos(nodoPadre->datosProducto->codigo,
+                                    nuevoNodo->datosProducto->codigo) > 0) {
+            nodoPadre->izq = nuevoNodo;
+        } else {
+            nodoPadre->der = nuevoNodo;
+        }
+        return 1;
+    }
+    return 0;
+}
+
 struct Lote *crearLote() {
     struct Lote *nuevoLote;
     struct Fecha *fechaCaducidad;
     int numeroLote, cantidadLote;
+
+    printf("Numero de lote: ");
     scanf("%d", &numeroLote);
+    printf("Cantidad de productos del lote: ");
     scanf("%d", &cantidadLote);
+    printf("Fecha de caducidad: ");
     fechaCaducidad = leerFecha();
+
     nuevoLote = (struct Lote *) malloc(sizeof(struct Lote));
     nuevoLote->numeroLote = numeroLote;
     nuevoLote->cantidadLote = cantidadLote;
@@ -180,6 +316,49 @@ struct NodoLote *crearNodoLote(struct Lote *lote) {
         nuevoNodo->sig = NULL;
     }
     return nuevoNodo;
+}
+
+struct Lote *getLote(struct NodoLote *head, int numeroLoteBuscado) {
+    struct NodoLote *rec;
+    if (head != NULL) {
+        rec = head;
+        while (rec != NULL) {
+            if (rec->datosLote->numeroLote == numeroLoteBuscado)
+                return rec->datosLote;
+            rec = rec->sig;
+        }
+    }
+    return NULL;
+}
+
+int agregarNodoLote(struct NodoLote **head, struct NodoLote *nuevoNodo) {
+    struct NodoLote *rec;
+    if (*head == NULL) {
+        *head = nuevoNodo;
+        return 1;
+    }
+    if (getLote(*head, nuevoNodo->datosLote->numeroLote) == NULL) {
+        rec = *head;
+        while (rec->sig != NULL) {
+            rec = rec->sig;
+        }
+        rec->sig = nuevoNodo;
+        return 1;
+    }
+    return 0;
+}
+
+int stockProducto(struct NodoLote *lotesProducto) {
+    struct NodoLote *rec;
+    int totalStock = 0;
+    if (lotesProducto != NULL) {
+        rec = lotesProducto;
+        while (rec != NULL) {
+            totalStock += rec->datosLote->cantidadLote;
+            rec = rec->sig;
+        }
+    }
+    return totalStock;
 }
 
 struct Producto **getArregloProductos(int totalProductos) {
@@ -199,6 +378,7 @@ struct CompraVenta *crearCompraVenta() {
     struct Fecha *fechaSolicitud;
     int cantidadProductos;
     char *nombre, estadoEnvio;
+
     scanf("%d", &cantidadProductos);
     scanf("%c ", &estadoEnvio);
     nombre = leerCadena();
@@ -222,12 +402,6 @@ struct NodoCompraVenta *crearNodoCompraVenta(struct CompraVenta *compraVenta) {
         nuevoNodo->sig = NULL;
     }
     return nuevoNodo;;
-}
-
-
-
-int buscarProducto() {
-
 }
 
 char *categoriaMasVendida() {
@@ -271,6 +445,14 @@ void mostrarInfoProducto() {
 }
 
 void analisisFarmacia() {
+
+}
+
+void menuFarmacia(struct Farmacia **farmacia) {
+
+}
+
+void menuFarmaSalud(struct FarmaSalud **farmaSalud) {
 
 }
 
